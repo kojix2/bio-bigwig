abaa25f307a52bcc338a40f76646be02a824d530
kojix2 <2xijok@gmail.com>
2021-12-18 16:12:47 +0900

    c extension subdirectory

--- ext/bio/bigwig/extconf.rb
+++ ext/bio/bigwig/extconf.rb
@@ -1,3 +1,11 @@
 require "mkmf"
 
+globs = [".", "libBigWig"].map do |directory|
+  File.join(File.dirname(__FILE__), directory)
+end.join(",")
+
+$objs = Dir.glob("{#{globs}}/*.c").map do |file|
+  File.join(File.dirname(file), "#{File.basename(file, ".c")}.o")
+end
+
 create_makefile("bio/bigwig/bigwigext")
